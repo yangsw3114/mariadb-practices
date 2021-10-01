9e81bd3caa50ef5f5fed8211cd390f398791b33a
yangsw3114 <72145880+yangsw3114@users.noreply.github.com>
2021-10-01 14:40:09 +0900

    commit

---
+++ sql연습/sql-practices/날짜함수.sql
@@ -0,0 +1,45 @@
+-- 함수: 날짜 함수
+
+-- CURDATE(), CURRENT_DATE
+select CURDATE(), CURRENT_DATE;
+
+-- CURTIME(), CURRENT_TIME
+select CURTIME(), CURRENT_TIME;
+
+-- now() vs sysdate()
+select now(), sysdate();
+select now(), sleep(2), now();
+select sysdate(), sleep(2), sysdate();
+
+-- date_format(date, format)
+select date_format(now(), '%Y년 %m월 %d일 %h시 %i분 %s초');
+select date_format(now(), '%Y년 %c월 %d일 %h시 %i분 %s초');
+
+-- period_diff
+-- YYMM, YYYYMM
+-- 예) 근무 개월 수를 출력
+  select first_name,
+         period_diff(date_format(curdate(), '%Y%m'), date_format(hire_date, '%Y%m')) as month
+    from employees
+order by month desc;
+
+-- date_add(= adddate), data_sub(= subdate)  
+-- 날짜를 date에 type(day, month, year) 형식의 표현식(expr) 더하거나 뺀다.
+-- 예제) 각 사원들의 근무 년수가 5년이 되는 날은 언제 인가요?
+select first_name,
+	   hire_date,
+       date_add(hire_date, interval 5 year)
+  from employees;
+
+-- cast
+select '12345' + 10, cast('12345' as int) + 10;
+select date_format(cast('2021-10-01' as date), '%Y-%m-%d');
+select cast(1-2 as unsigned);
+select cast(cast(1-2 as unsigned) as signed);
+
+-- mysql type
+-- varchar, char, text, CLOB(Character Large OBject)
+-- signed(unsigned), int(integer), medium int, big int, int(11)
+-- float, double
+-- time, date, datetime
+-- LOB: CLOB, BLOB
--- sql연습/sql-practices/수학함수.sql
+++ sql연습/sql-practices/수학함수.sql
@@ -16,3 +16,14 @@ select ceiling(3.14);
 -- round(x): x에 가장 근접한 정수
 -- round(x,d): x 값 중에 소수점 d자리에 가장 근접한 실수
 select round(1.298), round(1.498, 1), round(1.498, 0);
+
+
+-- pow(x, y), power(x, y) x의 y승
+select pow(2, 10), power(10, 2);
+
+-- sign(x)
+select sign(20), sign(-100), sign(0);
+
+-- greatest(x, y, .....), least(x, y, ....)
+select greatest(10, 40, 20, 30), least(10, 40, 20, 30);
+select greatest('b', 'A', 'C'), greatest('hello', 'hellp', 'hellq');
