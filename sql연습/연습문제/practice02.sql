90547fea829a8e329ca57246e7803cb75969f8ae
yangsw3114 <72145880+yangsw3114@users.noreply.github.com>
2021-10-06 09:49:23 +0900

    practice02

--- sql연습/연습문제/practice02.sql
+++ sql연습/연습문제/practice02.sql
@@ -9,16 +9,27 @@ from salaries;
 -- 문제2.
 -- 마지막으로 신입사원이 들어온 날은 언제 입니까? 다음 형식으로 출력해주세요.
 -- 예) 2014년 07월 10일
+select emp_no, date_format(max(hire_date),'%Y년 %m월 %d일') as '입사일' from employees;
+
 
 -- 문제3.
 -- 가장 오래 근속한 직원의 입사일은 언제인가요? 다음 형식으로 출력해주세요.
 -- 예) 2014년 07월 10일
+select emp_no, date_format(min(hire_date),'%Y년 %m월 %d일') as '입사일'
+from employees;
+
 
 -- 문제4.
 -- 현재 이 회사의 평균 연봉은 얼마입니까?
+select avg(salary) as '평균연봉' from salaries;
 
 -- 문제5.
 -- 현재 이 회사의 최고/최저 연봉은 얼마입니까?
+select max(salary) as '최고연봉', min(salary)as '최저연봉' from salaries;
+
 
 -- 문제6.
 -- 최고 어린 사원의 나이와 최 연장자의 나이는?
+select date_format(now(), '%Y') - date_format(max(birth_date),'%Y') as '어린사원',
+date_format(now(), '%Y') - date_format(min(birth_date),'%Y') as '최연장자사원'
+from employees;
